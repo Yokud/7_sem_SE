a0b0483790a85ba1e2be33c7143c06eb94f5455a
Ivan Malyshev <thefuny@mail.ru>
2022-09-27 00:00:05 +0300

    add labs

---
+++ IS/lab_01/UniqueApp/App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementObjectSearcher moSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+            var moObjs = moSearcher.Get().GetEnumerator();
+            moObjs.MoveNext();
+            string serialKey = moObjs.Current["SerialNumber"].ToString().Trim();
+
+            AppLib.App.Func(serialKey);
+
+            Console.Read();
+        }
+    }
+}
