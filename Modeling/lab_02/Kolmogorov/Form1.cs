cc2cb072b21151e91c42884275cf86d867193c4e
Ivan Malyshev <thefuny@mail.ru>
2022-10-28 01:52:11 +0300

    add lab2

---
+++ Modeling/lab_02/Kolmogorov/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Kolmogor;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int states;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ResultDataGridView.Columns.Add("ultProb", "Предельная вероятность");
+            ResultDataGridView.Columns.Add("statTime", "Точка стабилизации системы");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            states = int.Parse(StatesValue.Text);
+
+            if (states > KolmogorMath.MaxStatesCount)
+            {
+                MessageBox.Show("States must be less or equal " + KolmogorMath.MaxStatesCount);
+                MainDataGridView.Rows.Clear();
+                MainDataGridView.Columns.Clear();
+                return;
+            }
+
+            MainDataGridView.Rows.Clear();
+            MainDataGridView.Columns.Clear();
+
+            for (int i = 0; i < states; i++)
+            {
+                MainDataGridView.Columns.Add("P" + i, i.ToString());
+                MainDataGridView.Rows.Add();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Matrix<double> matrix = Matrix<double>.Build.Dense(states, states);
+
+            for (int i = 0; i < states; i++)
+            {
+                for (int j = 0; j < states; j++)
+                    matrix[i, j] = double.Parse(MainDataGridView.Rows[i].Cells[j].Value.ToString(), CultureInfo.InvariantCulture);
+            }
+
+            var probs = KolmogorMath.GetUltimatePropabilities(matrix);
+            var stats = KolmogorMath.GetStabilizationTimes(matrix, probs.EnumerateColumns().First()).ToArray();
+
+            ResultDataGridView.Rows.Clear();
+
+            for (int i = 0; i < states; i++)
+                ResultDataGridView.Rows.Add(probs[i, 0], stats[i]);
+        }
+    }
+}
---
+++ Modeling/lab_02/Kolmogorov/KolmogorovMath.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex;
+
+namespace Kolmogor
+{
+    internal static class KolmogorMath
+    {
+        public const double TimeDelta = 1e-3;
+        public const int MaxStatesCount = 10;
+
+        public static Matrix<double> GetUltimatePropabilities(Matrix<double> matrix)
+        {
+            var coefsMatrix = BuildCoefsMatrix(matrix);
+            var augmMatrix = BuildAugmentationMatrix(matrix.RowCount);
+
+            return coefsMatrix.Solve(augmMatrix);
+        }
+
+        public static IEnumerable<double> GetStabilizationTimes(Matrix<double> matrix, Vector<double> ultimatePropabilities)
+        {
+            int n = matrix.RowCount;
+            double currTime = 0;
+            Vector<double> currPropabilities = Vector<double>.Build.Dense(ultimatePropabilities.Count);
+            Vector<double> stabilizationTimes = Vector<double>.Build.Dense(n);
+
+            double totalLambdaSum = matrix.RowSums().Sum() * MaxStatesCount;
+            double[] Eps = ultimatePropabilities.Select(p => p / totalLambdaSum).ToArray();
+
+            while (!stabilizationTimes.All(p => Math.Abs(p) >= 1e-3))
+            {
+                var currDps = Dps(matrix, currPropabilities).ToArray();
+                for (int i = 0; i < n; i++)
+                {
+                    if (Math.Abs(stabilizationTimes[i]) < 1e-3 && currDps[i] <= Eps[i] && Math.Abs(currPropabilities[i] - ultimatePropabilities[i]) <= Eps[i])
+                        stabilizationTimes[i] = currTime;
+
+                    currPropabilities[i] += currDps[i];
+                }
+
+                currTime += TimeDelta;
+            }
+
+            return stabilizationTimes;
+        }
+
+        static IEnumerable<double> Dps(Matrix<double> matrix, Vector<double> probabilities)
+        {
+            int n = matrix.RowCount;
+
+            double[] res = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+
+                for (int j = 0; j < n; j++)
+                    sum += i != j ? probabilities[j] * matrix[j, i] : probabilities[j] * (matrix[i, i] - matrix.RowSums()[i]);
+                
+                res[i] = sum * TimeDelta;
+            }
+
+            return res;
+        }
+
+        static Matrix<double> BuildAugmentationMatrix(int count)
+        {
+            Matrix<double> matrix = Matrix<double>.Build.Dense(count, 1);
+            matrix[count - 1, 0] = 1;
+
+            return matrix;
+        }
+
+        static Matrix<double> BuildCoefsMatrix(Matrix<double> matrix)
+        {
+            Matrix<double> res = Matrix<double>.Build.Dense(matrix.RowCount, matrix.ColumnCount);
+            int n = matrix.RowCount;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    res[i, i] -= matrix[i, j];
+                for (int j = 0; j < n; j++)
+                    res[i, j] += matrix[j, i];
+            }
+
+            for (int i = 0; i < n; i++)
+                res[n - 1, i] = 1;
+
+            return res;
+        }
+    }
+}
