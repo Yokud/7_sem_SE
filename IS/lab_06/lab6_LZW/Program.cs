141bbbb6d0756f293ed721f9188a4b77f38025b6
Ivan Malyshev <thefuny@mail.ru>
2022-11-01 23:59:10 +0300

    add labs

---
+++ IS/lab_06/lab6_LZW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace lab6_LZW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fType = "rar";
+            LZWAlgo.Compress("test." + fType, "tmp.txt");
+            Console.WriteLine();
+            LZWAlgo.Decompress("tmp.txt", "res." + fType);
+        }
+    }
+}
